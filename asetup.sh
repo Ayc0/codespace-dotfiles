7a9a23a6383bea8908f2da5a6d7f7eb7fd11ee4b
Benjamin Koltes <ayc0.benj@gmail.com>
2022-10-31 22:25:14 +0100

    Update asetup.sh

--- asetup.sh
+++ asetup.sh
@@ -1,6 +1,8 @@
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+ZSH_CUSTOM="~/.oh-my-zsh/custom"
+
 # spaceship theme
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
